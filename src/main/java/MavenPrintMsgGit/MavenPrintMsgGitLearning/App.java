17ea06285d495e7fcc699899df7bec474ab2778d
shankdpk1 <shashank.deepak@gmail.com>
2017-11-20 22:40:20 -0500

    files added

---
+++ src/main/java/MavenPrintMsgGit/MavenPrintMsgGitLearning/App.java
@@ -0,0 +1,13 @@
+package MavenPrintMsgGit.MavenPrintMsgGitLearning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/MavenPrintMsgGit/MavenPrintMsgGitLearning/AppTest.java
@@ -0,0 +1,12 @@
+package MavenPrintMsgGit.MavenPrintMsgGitLearning;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+    @Test 
+	public void testApp()
+    {
+        System.out.println("hello");
+    }
+}
